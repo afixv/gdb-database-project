8ce95ba24dcef8cdf4e818cfa926f183dfe8624b
Afix Vega Praditya <afixv00@gmail.com>
2023-06-06 21:40:06 +0700

    feat: add 404 page

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export function NoPage() {
-    return <h1>404</h1>;
-  };
+  return (
+    <div className="text-center mt-24 align-middle">
+        <h1 className="text-3xl sm:text-5xl font-bold mb-2 text-dark-blue">404</h1>
+        <h2> Page Not Found </h2>
+    </div>
+  );
+}
